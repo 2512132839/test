f77b1f5a1bf4e9907c309e2012b1bcd9e43a3825
lzl <2512132839@qq.com>
2025-04-11 02:38:43 +0800

    1

---
+++ backend/src/services/index.js
@@ -0,0 +1,8 @@
+import * as adminService from "./adminService.js";
+import * as apiKeyService from "./apiKeyService.js";
+import * as s3ConfigService from "./s3ConfigService.js";
+import * as systemService from "./systemService.js";
+import * as pasteService from "./pasteService.js";
+import * as fileService from "./fileService.js";
+
+export { adminService, apiKeyService, s3ConfigService, systemService, pasteService, fileService };
---
+++ backend/src/utils/index.js
@@ -0,0 +1,9 @@
+/**
+ * 导出所有工具函数
+ */
+
+export * from "./common";
+export * from "./crypto";
+export * from "./fileUtils";
+export * from "./s3Utils";
+
